38c945f386376dc7a8906544cf824d28c7308847
Muhammad Fariz Tamara Asmarakusuma <muhammad.asmarakusuma@icbp.indofood.co.id>
2024-07-26 16:49:11 +0700

    added redux and implement it

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,19 @@ import App from './App.jsx'
 import './index.css'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { BrowserRouter } from "react-router-dom"; 
+import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
+
+import { Provider } from 'react-redux';
+import { rootStore as store } from './reducer/index.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
       <BrowserRouter basename='/'>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>,
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,6 +1,6 @@
 import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
 import { userReducer } from './userReducer'; 
-import  reduxThunk  from 'redux-thunk';
+import * as reduxThunk  from 'redux-thunk';
 export const rootStore = configureStore({
 
     //gabung semua reducer kesini
